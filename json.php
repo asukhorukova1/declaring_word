24380775adf6b87eb064c1fc9301f8d232e1a751
asukhorukova1 <asukhorukova@inbox.ru>
2017-05-15 17:47:36 +0200

    Update json.php

--- json.php
+++ json.php
@@ -10,13 +10,13 @@
    /* sanity check */
    if (json_decode($yes) != null)
    {
-     $file = fopen('https://github.com/asukhorukova1/declaring_word.git/data/yes/' . round(microtime(true) * 1000) . '_pamphletData.json','w+');
+     $file = fopen('data/yes/' . round(microtime(true) * 1000) . '_pamphletData.json','w+');
      fwrite($file, $yes);
      fclose($file);
    }
    else if (json_decode($no) != null)
    {
-     $file = fopen('https://github.com/asukhorukova1/declaring_word.git/data/no/' . round(microtime(true) * 1000) . '_pamphletData.json','w+');
+     $file = fopen('data/no/' . round(microtime(true) * 1000) . '_pamphletData.json','w+');
      fwrite($file, $no);
      fclose($file);
    }
